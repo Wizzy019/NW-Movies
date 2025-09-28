875be5f1ac719c65fc52a82de81750bbffcb8f62
Wizzy019 <kingwizzyndukwu@gmail.com>
2025-09-28 18:27:05 +0100

    Added and styled the navbar and its elements

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import "./Navbar.css";
+import Fire from '../../assets/fire.jpg'
+import Star from '../../assets/glowing-star.jpg'
+import Party from '../../assets/party-face.jpg'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <h1>MovieManiac</h1>
+
+        <div className="navbar-links">
+            <a href="">Popular <img src={Fire} alt="fire emoji" className='navbar-emoji'/></a>
+            <a href="">Top Rated <img src={Star} alt="star emoji" className='navbar-emoji'/></a>
+            <a href="">Upcoming <img src={Party} alt="party-face emoji" className='navbar-emoji'/></a>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar

89eff92a7fa4437ecec354ac30717528a593e329
Wizzy019 <kingwizzyndukwu@gmail.com>
2025-10-07 19:27:39 +0100

    Fix: Setup build and deploy to siut both vercel and github

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
+import process from 'process'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  base: '/.'
+export default defineConfig(({ mode }) => {
+  // Load all environment variables
+  const env = loadEnv(mode, process.cwd(), '')
+  const isVercel = env.VERCEL === '1'
+
+  return {
+    plugins: [react()],
+    base: isVercel ? '/' : '/NW-Movies/',
+  }
 })
